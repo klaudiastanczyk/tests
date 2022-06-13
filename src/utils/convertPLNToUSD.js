bbf7817403de3864f0b6793a6def3760afcc7cb3
Klaudia Stanczyk <klaudia.stanczyk92@gmail.com>
2022-06-13 23:43:39 +0200

    first tests

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,11 +1,24 @@
 export const convertPLNToUSD = (PLN) => {
 
-  const PLNtoUSD = PLN / 3.5;
-  
-  const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD'
-  });
+  if (typeof PLN === 'string' || PLN === undefined) {
+    return NaN; 
+  } else if (typeof PLN === 'number') {
 
-  return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+    if (PLN < 0) {
+      return "$0.00"
+    } else {
+        const PLNtoUSD = PLN / 3.5;
+        
+        const formatter = new Intl.NumberFormat('en-US', {
+          style: 'currency',
+          currency: 'USD'
+        });
+      
+        return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+    }
+    
+  } else {
+    return 'Error';
+  }
+
 }
